6d9ea28bc1500b2a2343e2eecbbc0fe77ccf0b9e
Kristjan <krebeljk@gmail.com>
2017-12-06 22:31:04 +0100

    add comments

--- calc.py
+++ calc.py
@@ -3,7 +3,9 @@
 import subprocess as sp
 import numpy as np
 
+# input param
 a = 7.8
+# computation
 com = "echo " + str(a**2) + " > result.txt"
-
+# result export
 sp.run(com, shell=True)
--- opt.py
+++ opt.py
@@ -4,13 +4,19 @@ import subprocess as supr
 import numpy as np
 from scipy.optimize import minimize
 
+# cost function
 def my_run(num):
+    # set parameter
     comProc = supr.run([r"sed -i 's/a\s*=\s*[^\s]*.*/a = " + str(num[0]) + "/g' calc.py"], shell=True)
+    # run external program 
     comProc = supr.run([r"python calc.py"], shell=True)
+    # read result
     return np.loadtxt('result.txt')
 
+# initial guess
 x0 = np.array([10])
 
+# optimization procedure
 res = minimize(my_run, x0, method='BFGS',options={'disp': True})
 
 print(res.x)
