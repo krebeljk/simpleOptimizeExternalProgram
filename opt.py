ba242946d6b5943f227b95c9a73dc1c24fd76070
Kristjan <krebeljk@gmail.com>
2017-12-06 21:28:54 +0100

    opt take parameter

--- opt.py
+++ opt.py
@@ -3,7 +3,9 @@
 import subprocess as sp
 import numpy as np
 
-comProc = sp.run([r"sed -i 's/a\s*=\s*[0-9]*\.[0-9]*/a = 7.8/g' calc.py"], shell=True)
+num = 12.2
+
+comProc = sp.run([r"sed -i 's/a\s*=\s*[0-9]*\.[0-9]*/a = " + str(num) + "/g' calc.py"], shell=True)
 comProc = sp.run([r"python calc.py"], shell=True)
 
 result = np.loadtxt('result.txt')
