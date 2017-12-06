2166b2dd192a66e47e53fff4ff4deae2fdaa80c8
Kristjan <krebeljk@gmail.com>
2017-12-06 21:31:31 +0100

    rename subprocess sp to supr

--- opt.py
+++ opt.py
@@ -1,11 +1,11 @@
 #!usr/bin/env python
 
-import subprocess as sp
+import subprocess as supr
 import numpy as np
 
 def my_run(num):
-    comProc = sp.run([r"sed -i 's/a\s*=\s*[0-9]*\.[0-9]*/a = " + str(num) + "/g' calc.py"], shell=True)
-    comProc = sp.run([r"python calc.py"], shell=True)
+    comProc = supr.run([r"sed -i 's/a\s*=\s*[0-9]*\.[0-9]*/a = " + str(num) + "/g' calc.py"], shell=True)
+    comProc = supr.run([r"python calc.py"], shell=True)
     return np.loadtxt('result.txt')
 
 my_run(30.1)
