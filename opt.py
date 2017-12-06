c5eeac4abc73665de9a599e3c5441bce05fa1a82
Kristjan <krebeljk@gmail.com>
2017-12-06 21:44:25 +0100

    opt deluje ampak bug

--- opt.py
+++ opt.py
@@ -2,10 +2,15 @@
 
 import subprocess as supr
 import numpy as np
+from scipy.optimize import minimize
 
 def my_run(num):
-    comProc = supr.run([r"sed -i 's/a\s*=\s*[0-9]*\.[0-9]*/a = " + str(num) + "/g' calc.py"], shell=True)
+    comProc = supr.run([r"sed -i 's/a\s*=\s*[0-9]*\.[0-9]*/a = " + str(num[0]) + "/g' calc.py"], shell=True)
     comProc = supr.run([r"python calc.py"], shell=True)
     return np.loadtxt('result.txt')
 
-my_run(30.1)
+x0 = np.array([10])
+
+res = minimize(my_run, x0, method='BFGS',options={'disp': True})
+
+print(res.x)
