44a7f5f02f48b186bb7c1cda90f63b52c1328a59
Kristjan <krebeljk@gmail.com>
2017-12-06 21:50:33 +0100

    napravi eno iteracijo brez errorjev

--- opt.py
+++ opt.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.optimize import minimize
 
 def my_run(num):
-    comProc = supr.run([r"sed -i 's/a\s*=\s*[0-9]*\.[0-9]*/a = " + str(num[0]) + "/g' calc.py"], shell=True)
+    comProc = supr.run([r"sed -i 's/a\s*=\s*[^\s]*&/a = " + str(num[0]) + "/g' calc.py"], shell=True)
     comProc = supr.run([r"python calc.py"], shell=True)
     return np.loadtxt('result.txt')
 
